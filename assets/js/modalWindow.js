e8cf713a8890ca43d88a65d57a8eda5c011fb82e
rshevchuk0 <55762378+rshevchuk0@users.noreply.github.com>
2020-05-11 01:46:30 +0300

    Penultimate change 7

--- assets/js/modalWindow.js
+++ assets/js/modalWindow.js
@@ -1,32 +1,27 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var modal = document.getElementById("mdl");
 
-
-// Get the modal
-var cuisine_classic = document.getElementById("cuisineClassic");
-
-// Get the button that opens the modal
-var btn1 = document.getElementById("classic-menu");
+function stopDefAction(e) {
+    e.preventDefault();
+    if (e.target != e.currentTarget) {
+        var clicked = e.target.title;
+        modal.style.display = "block";
+        console.log('click event triggered at ' + clicked);
+    }
+}
+var map = document.querySelector("map").addEventListener(
+    'click', stopDefAction, false
+);
+var img0 = document.getElementById("rm1");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-window")[0];
-
-// When the user clicks on the button, open the modal
-btn1.onclick = function() {
-  cuisine_classic.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  cuisine_classic.style.display = "none";
-}
+var span1 = document.getElementsByClassName("close-window")[0];
+span1.onclick = function(){
+    modal.style.display = "none";
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target === cuisine_classic) {
-    cuisine_classic.style.display = "none";
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
